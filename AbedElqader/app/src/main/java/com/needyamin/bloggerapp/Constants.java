2d4a2a4a5ba6d826bfeefe2102f77c396a2fbb90
abedkahlout <49364392+abedkahlout@users.noreply.github.com>
2022-06-07 19:48:26 +0300

    first commit

---
+++ AbedElqader/app/src/main/java/com/needyamin/bloggerapp/Constants.java
@@ -0,0 +1,12 @@
+package com.needyamin.bloggerapp;
+
+public class Constants {
+
+
+    public static final String API_KEY ="AIzaSyDZwavzLfyNQdXD8_0pnL5tEiZ7PxZN6Ns";
+    public static final String BLOG_ID="585610768357052861";
+    public static final  String MAX_POST_RESULTS = "10";
+}
+
+
+
